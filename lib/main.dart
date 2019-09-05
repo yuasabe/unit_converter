556b9adb61af8f3dd0264ac66a0e40c7bda1cff2
Yu Asabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2019-09-05 15:00:51 +0900

    finished quiz 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:hello_rectangle/category.dart';
+import 'package:hello_rectangle/category_route.dart';
 
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
+//const _categoryName = 'Cake';
+//const _categoryIcon = Icons.cake;
+//const _categoryColor = Colors.green;
 
 void main() {
   runApp(UnitConverterApp());
@@ -16,16 +16,7 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     );
   }
 }
